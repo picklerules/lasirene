405177e10225d75294296e0d8ccd84367df71faf
picklerules <136508588+picklerules@users.noreply.github.com>
2023-08-26 16:14:11 -0400

    page service

--- assets/js/theme_couleur.js
+++ assets/js/theme_couleur.js
@@ -1,19 +1,31 @@
 function changeTheme(chakra) {
-    // Mapping des noms de chakra aux variables CSS
-    const chakraColors = {
-      'root': '--chakra-root',
-      'sacral': '--chakra-sacral',
-      'solar-plexus': '--chakra-solar-plexus',
-      'heart': '--chakra-heart',
-      'throat': '--chakra-throat',
-      'third-eye': '--chakra-third-eye',
-      'crown': '--chakra-crown'
-    };
-  
-    // Récupération de la couleur correspondante
-    const selectedColor = chakraColors[chakra];
-  
-    // Application de la couleur sélectionnée
-    document.documentElement.style.setProperty('--chakra-selected', `var(${selectedColor})`);
-  }
-  
+  // Mapping des noms de chakra aux variables CSS pour les couleurs principales
+  const chakraColors = {
+    'root': '--chakra-root',
+    'sacral': '--chakra-sacral',
+    'solar-plexus': '--chakra-solar-plexus',
+    'heart': '--chakra-heart',
+    'throat': '--chakra-throat',
+    'third-eye': '--chakra-third-eye',
+    'crown': '--chakra-crown'
+  };
+
+  // Mapping des noms de chakra aux variables CSS pour les couleurs d'arrière-plan
+  const chakraBgColors = {
+    'root': '--chakra-root-bg',
+    'sacral': '--chakra-sacral-bg',
+    'solar-plexus': '--chakra-solar-plexus-bg',
+    'heart': '--chakra-heart-bg',
+    'throat': '--chakra-throat-bg',
+    'third-eye': '--chakra-third-eye-bg',
+    'crown': '--chakra-crown-bg'
+  };
+
+  // Récupération des couleurs correspondantes
+  const selectedColor = chakraColors[chakra];
+  const selectedBgColor = chakraBgColors[chakra];
+
+  // Application des couleurs sélectionnées
+  document.documentElement.style.setProperty('--chakra-selected', `var(${selectedColor})`);
+  document.documentElement.style.setProperty('--chakra-selected-bg', `var(${selectedBgColor})`);
+}
